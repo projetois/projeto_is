307fa745b3734fca4c9055299f126053f217cc13
Joao Rosario <joao.rosario.exb@gmail.com>
2016-12-18 23:10:30 +0000

    refwsfedsa

---
+++ SmartH2O_Service/SmartH20_Service.svc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+
+namespace SmartH2O_Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class SmartH2O_Service : DLogger
+    {
+        public string saveAndFormatData(string data)
+        {
+           
+            return "test";
+            //Verificar se XML recebido tem elemento "message" para distinguir ficheiro datauploader ou ficheiro alarme
+
+            
+
+           
+        }
+
+        private void verifyParamOrAlarm(XmlDocument xml)
+        {
+            XmlNodeList message = xml.GetElementsByTagName("message");
+           // File.AppendAllText(Directory.GetCurrentDirectory() + "\\" + filename, data);
+            /* if (message.Count == 0)
+             {
+                 return "param";
+             }
+             else
+             {
+                 return "alarm";
+             }*/
+        } 
+    }
+
+
+
+}
+
